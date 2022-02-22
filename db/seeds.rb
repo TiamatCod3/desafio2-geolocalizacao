03a33187e0b73d9e1c9d814cda7ec24c9df1e704
TiamatCod3 <ti_matos@id.uff.br>
2022-02-21 23:49:50 -0300

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+url = "http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/estacoesBikeRio"
+
+data = RestClient.get url
+
+bike_points = JSON.parse(data)['DATA']
+
+bike_points.each |point| do
+  
+end
